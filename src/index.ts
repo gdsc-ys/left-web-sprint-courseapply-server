5c0dd6d3b50e81c293f9e9f4cfe2b4e29595eb08
namooplus <namoo.developer@gmail.com>
2022-11-27 17:31:12 +0900

    feat: typescript configuration

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("Hi");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
 });
