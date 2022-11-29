4c0f2986676fd02b6a3a10fcea40a80c6218eda6
namooplus <namoo.developer@gmail.com>
2022-11-29 23:57:31 +0900

    feat: initialize firestore

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -1,8 +1,23 @@
-import { initializeApp, credential, type ServiceAccount } from "firebase-admin";
+import { initializeApp, cert, type ServiceAccount } from "firebase-admin/app";
+import { getFirestore, type Firestore } from "firebase-admin/firestore";
 import serviceAccount from "../data/serviceAccount.json";
+
+let db: Firestore;
 
 export const initializeFirebase = () => {
   initializeApp({
-    credential: credential.cert(serviceAccount as ServiceAccount),
+    credential: cert(serviceAccount as ServiceAccount),
   });
+
+  db = getFirestore();
+};
+
+const validateDB = () => {
+  if (!db) {
+    throw Error("Database is NOT intialized!");
+  }
+};
+
+export const postData = () => {
+  validateDB();
 };
