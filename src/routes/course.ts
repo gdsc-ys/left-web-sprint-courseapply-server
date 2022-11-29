6a85cdce7094e39f4eeaa4ed7ee8723f1a7a12e2
namooplus <namoo.developer@gmail.com>
2022-11-29 23:10:03 +0900

    feat: add api request and response interface

--- src/routes/course.ts
+++ src/routes/course.ts
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import { Course } from "../interfaces/Course";
 
 const courseRouter = Router();
 
 /**
  * 강의 목록 가져오기
  */
+export interface GetCoursesRequest
+  extends Partial<Pick<Course, "degree" | "college" | "major">> {}
+
+export interface GetCoursesResponse extends Array<Course> {}
+
 courseRouter.get("/", (req, res) => {
   res.send("Course List");
 });
@@ -12,6 +18,13 @@ courseRouter.get("/", (req, res) => {
 /**
  * 단과대 및 전공 목록 가져오기
  */
+export type GetCollegesAndMajorsRequest = void;
+
+export interface GetCollegesAndMajorsResponse {
+  colleges: string[];
+  majors: string[];
+}
+
 courseRouter.get("/colleges-and-majors", (req, res) => {
   res.send("Colleges and Majors");
 });
@@ -19,6 +32,10 @@ courseRouter.get("/colleges-and-majors", (req, res) => {
 /**
  * 강의 하나 가져오기
  */
+export interface GetCourseRequest extends Pick<Course, "id"> {}
+
+export interface GetCourseResponse extends Course {}
+
 courseRouter.get("/:id", (req, res) => {
   const courseId = req.params.id;
 
--- src/routes/mycourse.ts
+++ src/routes/mycourse.ts
@@ -1,10 +1,15 @@
 import { Router } from "express";
+import { Course } from "../interfaces/Course";
 
 const myCourseRouter = Router();
 
 /**
  * 수강 신청
  */
+export interface ApplyRequest extends Pick<Course, "id"> {}
+
+export type ApplyResponse = Course[];
+
 myCourseRouter.post("/", (req, res) => {
   res.send("Applied");
 });
@@ -12,6 +17,10 @@ myCourseRouter.post("/", (req, res) => {
 /**
  * 수강 취소
  */
+export interface WithdrawRequest extends Pick<Course, "id"> {}
+
+export type WithdrawResponse = Course[];
+
 myCourseRouter.delete("/:id", (req, res) => {
   const courseId = req.params.id;
   res.send(`${courseId} Withdrew`);
@@ -20,6 +29,10 @@ myCourseRouter.delete("/:id", (req, res) => {
 /**
  * 신청 내역 목록 가져오기
  */
+export type GetAppliedCoursesRequest = void;
+
+export interface GetAppliedCoursesResponse extends Array<Course> {}
+
 myCourseRouter.get("/", (req, res) => {
   res.send("My course list");
 });
