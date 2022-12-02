527c85eaf4bebb95aa4aeb228547e0a9490c6dea
namooplus <namoo.developer@gmail.com>
2022-12-02 23:11:35 +0900

    feat: use 8080 port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import myCourseRouter from "./routes/mycourse";
 import { initializeFirebase } from "./utils/firebase";
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
